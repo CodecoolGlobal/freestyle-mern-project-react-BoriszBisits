415d022c63134b9430bced78e08ea07184077eea
DJHeadset <115445237+DJHeadset@users.noreply.github.com>
2023-04-04 16:14:46 +0200

    bugfix

--- client/src/components/Characters.jsx
+++ client/src/components/Characters.jsx
@@ -4,14 +4,11 @@ function Characters({ characters, onCharacterDetails }) {
   return (
     <div className="container">
       {characters.map((character, i) => character.isAlive && (
-        
-        <div className="chatactercards" key={i}>
-          <div className={`cahfamily ${character.family.split(' ')[1]}`}>
+        <div className={`chatactercards ${character.family.split(' ')[1]}`}>
           <h2>{character.name}</h2>
           <h3>{character.title}</h3>
           <img  src={character.imgurl} alt={character.img}></img>
-          <button onClick={() => onCharacterDetails(character)}>Character Details</button>
-          </div>
+          <button onClick={() => onCharacterDetails(character)}>Character Details</button>      
         </div>
       ))}
     </div>
