7f8f2069d0c4430733cafc991d44fbf29860ebb0
adam.metal <adam.metal@codecool.com>
2023-04-06 13:48:30 +0200

    effect reviewed

--- client/src/components/Council.jsx
+++ client/src/components/Council.jsx
@@ -1,32 +1,39 @@
-import React, { useEffect, useState } from 'react'
-import '../App.css';
+import React, { useEffect, useState } from "react";
+import "../App.css";
 
 function Council({ characters, onKill }) {
-
-  const [myCouncil, setMyCouncil] = useState([])
-
-  function getCouncil() {
-    setMyCouncil(characters.filter(char =>
-      char.councilMember === true && char.isAlive === true
-    ))
-  }
+  const [myCouncil, setMyCouncil] = useState([]);
 
   useEffect(() => {
-    getCouncil()
-  }, [])
+    setMyCouncil(
+      characters.filter(
+        (char) => char.councilMember === true && char.isAlive === true
+      )
+    );
+  }, [characters]);
 
   return (
-    <div className='container'>
-      {myCouncil && myCouncil.map((member, i) => (
-        <div key={i} className={`chatactercards ${member.family.split(' ')[1]}`}>
-        <h2>{member.name}</h2>
-        <h3>{member.title}</h3>
-        <img  src={member.imgurl} alt={member.img}></img>
-        <button className='header-btn'  key={`btn${i}`} onClick={()=>onKill(member)}>Kill</button>      
-      </div>
-      ))}
+    <div className="container">
+      {myCouncil &&
+        myCouncil.map((member, i) => (
+          <div
+            key={i}
+            className={`chatactercards ${member.family.split(" ")[1]}`}
+          >
+            <h2>{member.name}</h2>
+            <h3>{member.title}</h3>
+            <img src={member.imgurl} alt={member.img}></img>
+            <button
+              className="header-btn"
+              key={`btn${i}`}
+              onClick={() => onKill(member)}
+            >
+              Kill
+            </button>
+          </div>
+        ))}
     </div>
   );
-};
+}
 
-export default Council
+export default Council;
